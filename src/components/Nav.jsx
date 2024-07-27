20325600ab6c06cecfbc41adce3bed2ee8d2d050
tokosaniya09 <tokosaniya09@gmail.com>
2024-07-27 09:22:14 +0530

    Basic layout of nike website

---
+++ src/components/Nav.jsx
@@ -0,0 +1,42 @@
+import { headerLogo } from '../assets/images';
+import { hamburger } from '../assets/icons';
+import { navLinks } from '../constants/index';
+
+const Nav = () => {
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-container'>
+            <a href="/">
+                <img 
+                src={headerLogo} 
+                    alt="Logo" 
+                    width={130} 
+                    height={29}
+                    className='m-0 w-[129px] h-[29px]'
+                />
+            </a>
+            <ul  className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+                {navLinks.map((item) => (
+                    <li key={item.label}>
+                        <a 
+                            href="item.href" 
+                            className='font-monts errat leading-normal text-lg text-slate-gray'
+                        >
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            <img 
+                className="block lg:hidden" 
+                src={hamburger} 
+                alt="hamburger icon" 
+                width={25} 
+                height={25}
+            />
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
---
+++ src/section/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+    Hero, 
+    PopularProducts, 
+    Services, 
+    SpecialOffer, 
+    Subscribe, 
+    SuperQuality, 
+    CustomerReviews, 
+    Footer
+}
